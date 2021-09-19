0d3e78590cefa9ceee282032277b339c84d07279
K Flora <56885337+KiloLimaFoxtrot@users.noreply.github.com>
2021-09-18 20:52:01 -0700

    Project update
    Project update

--- Work_01/src/Part01/part01Workv01.java
+++ Work_01/src/Part01/part01Workv01.java
@@ -1,4 +1,6 @@
-public class test01 {
+package Part01;
+
+public class part01Workv01 {
 	public static void main(String[] args) {
 		System.out.println("\nCoursera Algorithms Project Hello " +
 		                   "World Test 01");
